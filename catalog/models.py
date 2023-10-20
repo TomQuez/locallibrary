1804e70172002c078ad5bb23c48c31984e7e5fb4
TomQuez <tom.quez@live.fr>
2023-10-20 14:57:19 +0200

    ajout modele Author

--- catalog/models.py
+++ catalog/models.py
@@ -47,3 +47,20 @@ class BookInstance(models.Model):
     def __str__(self):
         """Fonction requise par django pour manipuler les objets Book dans la base de données."""
         return f'{self.id}({self.book.title})'
+    
+class Author(models.Model):
+    """Model représentant un Auteur"""
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    date_of_birth=models.DateField(null=True,blank=True)
+    date_of_death=models.DateField('Died',null=True,blank=True)
+    
+    class Meta:
+        ordering=['last_name','first_name']
+        
+    def get_absolute_url(self):
+        """Return the url to access a particular author instance."""
+        return reverse('author-detail',args=[str(self.id)])
+    def __str__(self):
+        """String for representing the Model object"""
+        return f'{self.last_name},{self.first_name}'
