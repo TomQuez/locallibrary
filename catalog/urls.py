deb2aa3735331b293d8b61f8684b330c7a9367d3
TomQuez <tom.quez@live.fr>
2023-10-23 13:29:00 +0200

    codage de la page d'accueil

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-urlpattern=[
+urlpatterns=[
     path('',views.index,name='index'),
+    path('books/',views.BookListView.as_view(),name='books'),
 ]
 
--- catalog/views.py
+++ catalog/views.py
@@ -10,11 +10,12 @@ def index(request):
     
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.all().count()
-    
+    num_genre=Genre.objects.all().count()
     context={
         'num_books':num_books,
         'num_instances':num_instances,
         'num_instances_available':num_instances_available,
-        'num_authors':num_authors
+        'num_authors':num_authors,
+        'num_genre':num_genre,
     }
     return render(request,'index.html',context=context)
