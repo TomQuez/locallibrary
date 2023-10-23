6eb9fc24b4dbfd27dfa731c16d991758d26eb434
TomQuez <tom.quez@live.fr>
2023-10-23 08:37:50 +0200

    ajout Model language, field language dans Book, et migration bdd

--- catalog/models.py
+++ catalog/models.py
@@ -11,6 +11,13 @@ class Genre(models.Model):
         """cette méthode surcharge __str__ et est obligatoirement requise par django. Elle retounre une chaine de caractère pour identifier l'instance de la classe d'objet"""
         return self.name
     
+class Language(models.Model):
+    """Model representing a language(e.g. English, French, Japanese,etc)."""
+    name=models.CharField(max_length=200,help_text="Enter the book's natural language(e.e. Englis, Franch, Japanese,etc...)")
+    def __str__(self):
+        """string for representing the Model object(in admin site)"""
+        return self.name
+
 
 class Book(models.Model):
     """cet objet représente un livre (mais ne traite pas les copies disponible en rayon pour le prêt)"""
@@ -19,10 +26,20 @@ class Book(models.Model):
     summary=models.TextField(max_length=1000,help_text='Enter a brief description of the book')
     isbn=models.CharField('ISBN',max_length=13,help_text='13 Characters <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre=models.ManyToManyField(Genre,help_text='Select a genre for this book')
+    language=models.ForeignKey(Language,on_delete=models.SET_NULL, null=True)
+    class Meta:
+        ordering=['title','author']
+    
+    def display_genre(self):
+        """Creates a string for the Genre.this is required to display genre in Admin."""
+        return ','.join([genre.name for genre in self.genre.all()[:3]])
+    
+    display_genre.short_description='Genre'
     
     def __str__(self):
         """fonction requise par django pour manipuler les objets Book dans la base de données."""
         return self.title
+    
     
     def get_absolute_url(self):
         """Cette fonction est requise par django, lorsque vous souhaitez détailler le contenu d'un objet."""
