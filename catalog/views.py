d095fe314ef08f1b51bd2b5c752467b44f9ce1dd
TomQuez <tom.quez@live.fr>
2023-10-23 15:07:35 +0200

    ajout vue générique liste de livre et vue detail de livre

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
 from catalog.models import Book,Author,BookInstance,Genre
 
@@ -19,3 +19,10 @@ def index(request):
         'num_genre':num_genre,
     }
     return render(request,'index.html',context=context)
+
+class BookListView(generic.ListView):
+    model=Book
+   
+    
+class BookDetailView(generic.DetailView):
+    model=Book
